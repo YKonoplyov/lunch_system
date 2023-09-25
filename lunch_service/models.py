31d8c24f3356648bc2c6e3846423c1e41a6a0c76
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 16:33:06 +0300

    created Restaurant model

--- lunch_service/models.py
+++ lunch_service/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
