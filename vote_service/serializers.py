2958cb99bea23bf5e35e9ec9961425e5fb81fd97
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 20:34:32 +0300

    created VoteResultsSerializer

--- vote_service/serializers.py
+++ vote_service/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, Serializer
 
 from vote_service.models import Votes
 
@@ -7,3 +8,8 @@ class VoteSerializer(ModelSerializer):
     class Meta:
         model = Votes
         exclude = ("date_voted", "employee")
+
+
+class VoteResultsSerializer(Serializer):
+    menu_name = serializers.CharField(max_length=255)
+    votes_count = serializers.IntegerField()
