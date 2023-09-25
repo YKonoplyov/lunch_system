0fd80d4b2df4519d7d85be2f9f54b35848cf6b2a
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 15:54:40 +0300

    created user creation view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class CreateUserView(CreateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = []
