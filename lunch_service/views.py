fc011ea03f88caa216a23822493ca8f1941ea17f
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 18:02:57 +0300

    created DishesViewSet

--- lunch_service/views.py
+++ lunch_service/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-from lunch_service.models import Restaurant
-from lunch_service.serializers import RestaurantSerializer, TagsSerializer
+from lunch_service.models import Restaurant, Dishes, Menus
+from lunch_service.serializers import (
+    RestaurantSerializer,
+    TagsSerializer,
+    DishSerializer,
+)
 
 
 class RestaurantCreateView(CreateAPIView):
@@ -13,3 +17,6 @@ class RestaurantCreateView(CreateAPIView):
 
 class TagCreateView(CreateAPIView):
     serializer_class = TagsSerializer
+class DishesViewSet(ModelViewSet):
+    serializer_class = DishSerializer
+    queryset = Dishes.objects.all()
