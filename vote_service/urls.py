bcb708dad5b6bc04f315b0de4f5db4a735fe4df5
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 20:40:09 +0300

    added GetVoteResultsView to vote_service urls

--- vote_service/urls.py
+++ vote_service/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from vote_service.views import VoteCreateView
+from vote_service.views import VoteCreateView, GetVoteResultsView
 
 urlpatterns = [
-    path("vote", VoteCreateView.as_view(), name="make_vote")
+    path("vote/", VoteCreateView.as_view(), name="make_vote"),
+    path("results/", GetVoteResultsView.as_view(), name="voting_results")
 ]
 
 app_name = "vote-service"
