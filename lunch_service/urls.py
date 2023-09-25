a66ec0117c3645f4ef875a038ece22011b82e1df
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 18:05:38 +0300

    included lunch_service_router urls to lunch_service urls

--- lunch_service/urls.py
+++ lunch_service/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from lunch_service.views import RestaurantCreateView, TagCreateView
+from lunch_service.views import (
+    RestaurantCreateView,
+    TagCreateView,
     DishesViewSet,
+
 lunch_service_router = routers.DefaultRouter()
 lunch_service_router.register("dishes", DishesViewSet)
+lunch_service_router.register("menus", MenuViewSet)
 
 urlpatterns = [
     path("restaurants/", RestaurantCreateView.as_view(), name="restaurants"),
     path("tags/", TagCreateView.as_view(), name="tags"),
+    path("", include(lunch_service_router.urls))
 ]
 
 app_name = "lunch-service"
