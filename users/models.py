28651646c9e5cce140f8bab48ab59de49198a613
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 14:27:22 +0300

    created custom user model Employee

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Employee(AbstractUser):
+    username = None
+    email = models.EmailField("email address", unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
