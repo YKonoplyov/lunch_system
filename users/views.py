5ab86904e5c9018a8608f0046f7ecf6abeb54f91
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 16:15:56 +0300

    crated ManageUserView

--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveUpdateAPIView,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from users.serializers import UserSerializer
 
@@ -7,3 +12,13 @@ from users.serializers import UserSerializer
 class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
     authentication_classes = []
+
+
+class ManageUserView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
