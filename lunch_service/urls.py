a9db2139a34d99f23212aa4314487e6aa921537d
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 18:04:46 +0300

    registered DishesViewSet to lunch_service router

--- lunch_service/urls.py
+++ lunch_service/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from lunch_service.views import RestaurantCreateView, TagCreateView
+    DishesViewSet,
 lunch_service_router = routers.DefaultRouter()
+lunch_service_router.register("dishes", DishesViewSet)
 
 urlpatterns = [
     path("restaurants/", RestaurantCreateView.as_view(), name="restaurants"),
