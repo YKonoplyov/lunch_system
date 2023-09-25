42473ff61908bad7020c2783cd5f4582295dae98
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 18:50:58 +0300

    created Votes model

--- vote_service/models.py
+++ vote_service/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from lunch_service.models import Menus
+from users.models import Employee
+
+
+class Votes(models.Model):
+    employee = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="vote"
+    )
+    menu = models.ForeignKey(
+        Menus,
+        on_delete=models.CASCADE,
+        related_name="menu")
+    date_voted = models.DateField(
+        auto_now_add=True
+    )
+
+
