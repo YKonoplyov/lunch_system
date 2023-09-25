3d02e772f6295d9627c4508ba280c71360cb7efc
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 17:00:26 +0300

    created views for creating Restaurants and Tags

--- lunch_service/views.py
+++ lunch_service/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from lunch_service.models import Restaurant
+from lunch_service.serializers import RestaurantSerializer, TagsSerializer
+
+
+class RestaurantCreateView(CreateAPIView):
+    serializer_class = RestaurantSerializer
+
+
+class TagCreateView(CreateAPIView):
+    serializer_class = TagsSerializer
