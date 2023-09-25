2a7c024d99c1b68f8df29c63d623848cf7bd860a
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 18:51:51 +0300

    created VoteCreateView

--- vote_service/views.py
+++ vote_service/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from vote_service.serializers import VoteSerializer
+
+
+class VoteCreateView(CreateAPIView):
+    serializer_class = VoteSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        print(self.request)
+        serializer.save(employee=self.request.user)
