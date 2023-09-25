b8f442481e0489a3b1e04f240ed1abc2d762e680
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 16:59:19 +0300

    created Tags model

--- lunch_service/models.py
+++ lunch_service/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
