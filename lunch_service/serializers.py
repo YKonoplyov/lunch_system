55a6680d1810f7f27f01a092bb36628500cbe839
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-09-25 17:02:04 +0300

    fixed Tags model import to serializers.py

--- lunch_service/serializers.py
+++ lunch_service/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from lunch_service.models import Restaurant
+from lunch_service.models import Restaurant, Tags
 
 
 class RestaurantSerializer(ModelSerializer):
